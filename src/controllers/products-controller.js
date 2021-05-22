b50c05d87e58ad095b22422971cac80b84ec5ac1
Dias Malibekov <pika4u7@gmail.com>
2021-05-22 09:00:36 +0500

    pagination

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -4,30 +4,34 @@ const httpError = require('../models/http-error')
 const Product = mongoose.model('Product')
 
 const getProducts = async (req, res) => {
-	const products = await Product.find()
-	res.send(products)
+  const page = parseInt(req.query.page)
+  const products = await Product
+    .find()
+    .skip((5 * page) - 5)
+    .limit(5)
+  res.send(products)
 }
 
 const createProduct = async (req, res) => {
-	const {title, price, image, salesTime, needToBuy, bought, vendorCode} = req.body
-	const product = new Product({
-		title,
-		price, 
-		image, 
-		salesTime, 
-		needToBuy,
-		bought, 
-		vendorCode
-	})
-	await product.save()
+  const {title, price, image, salesTime, needToBuy, bought, vendorCode} = req.body
+  const product = new Product({
+    title,
+    price,
+    image,
+    salesTime,
+    needToBuy,
+    bought,
+    vendorCode
+  })
+  await product.save()
 
-	res.json({message: 'New product successfully created.'})	
+  res.json({message: 'New product successfully created.'})
 }
 
 const getProductById = async (req, res) => {
-	const id = req.params.pid 
-	const product = await Product.findById(id)
-	res.send(product)
+  const id = req.params.pid
+  const product = await Product.findById(id)
+  res.send(product)
 
 }
 
